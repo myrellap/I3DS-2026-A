b87c57895a50be1374a2fcc80ac3ff4fff03448b
myrellap <myrellaputti2007@gmail.com>
2026-02-12 10:15:20 -0300

    Tradução do commit: // feat: inicializar o projeto 03-devlinks com Vite e React // // - Adicionar package.json com dependências e scripts para desenvolvimento, build e linting. // - Incluir o SVG do logo do Vite no diretório público. // - Criar App.css para estilizar a aplicação. // - Implementar App.jsx como o componente principal da aplicação com hooks do React. // - Adicionar o SVG do logo do React no diretório de assets. // - Criar index.css para estilos globais e temas. // - Configurar main.jsx para renderizar o componente App. // - Configurar o Vite com um arquivo básico vite.config.js.

--- 02-react-vite/vite.config.js
+++ 02-react-vite/vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-   port: 3000, //muda o padrao vite(5173) para o padrao react(3000)
-   open: true, //Abra o navegador automaticamente
-   host: true, // permite que tenha acesso via IP na rede local
+    port: 3000, //muda o padrao vite(5173) para o padrao react(3000)
+    open: true, //Abra o navegador automaticamente
+    host: true, // permite que tenha acesso via IP na rede local
   },
 });
+
