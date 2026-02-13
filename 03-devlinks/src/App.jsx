2bc5df7bb9aa9845114ca9a7cacca882a3b32495
myrellap <myrellaputti2007@gmail.com>
2026-02-13 11:03:04 -0300

    refactor: reorganizar estrutura do componente Link e adicionar estilos

--- 03-devlinks/src/App.jsx
+++ 03-devlinks/src/App.jsx
@@ -14,14 +14,17 @@ function App() {
 
        <div className="switch">botão switch</div>
 
-      <ul>
+      <div id="Link">
+        <ul>
         <Link url={""}>Inscreva-se</Link>
         <Link url={""}>Minha Playlist</Link>
         <Link url={""}>Me pague um lanche</Link>
         <Link url={""}>Conheça o curso de DEV</Link>
-      </ul>
+        </ul>
+      </div>
 
-      <div className='socialLinks'></div>
+
+      <div id='socialLinks'></div>
       <SocialLink url={"https://github.com"} icon={"logo-github"} />
       <SocialLink url={"https://instagram.com"} icon={"logo-instagram"} />
       <SocialLink url={"https://youtube.com"} icon={"logo-youtube"} />
