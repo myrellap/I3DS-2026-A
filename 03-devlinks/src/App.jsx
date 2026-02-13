24515b269fb61e0a9452425962eac4e73dccb3ba
myrellap <myrellaputti2007@gmail.com>
2026-02-13 16:12:30 -0300

    refactor: reorganizar o componente Link e adicionar o componente Switch com estilos

--- 03-devlinks/src/App.jsx
+++ 03-devlinks/src/App.jsx
@@ -2,40 +2,40 @@
 import './App.css'
 import Link from './assets/components/Link/Link';
 import Perfil from './assets/components/Perfil/Perfil';
-import Rodape from './assets/components/Rodape/Rodape';
 import SocialLink from './assets/components/SocialLink/SocialLink';
+import Rodape from './assets/components/Rodape/Rodape';
+import Switch from './assets/components/Switch/Switch';
+
 
 function App() {
 
   return (
    <div id= "App">
- <Perfil fotoPerfil={"https://placehold.co/100"}>Myrella Putti </Perfil>
+ <Perfil fotoPerfil={"https://placehold.co/100"}>Myrella</Perfil>
 
 
-       <div className="switch">botão switch</div>
+       <Switch />
 
-      <div id="Link">
-        <ul>
+<div id= "Link">
+      <ul>
         <Link url={""}>Inscreva-se</Link>
         <Link url={""}>Minha Playlist</Link>
         <Link url={""}>Me pague um lanche</Link>
         <Link url={""}>Conheça o curso de DEV</Link>
-        </ul>
+      </ul>
       </div>
-
 
       <div id='SocialLinks'>
+       <SocialLink url={"https://github.com"} icon = {"logo-github"} />
+       <SocialLink url={"https://instagram.com"} icon = {"logo-instagram"} />
+       <SocialLink url={"https://youtube.com"} icon = {"logo-youtube"} />
+       <SocialLink url={"https://br.linkedin.com/"} icon = {"logo-linkedin"} />
+       </div>
 
-      <SocialLink url={"https://github.com"} icon={"logo-github"} />
-      <SocialLink url={"https://instagram.com"} icon={"logo-instagram"} />
-      <SocialLink url={"https://youtube.com"} icon={"logo-youtube"} />
-      <SocialLink url={"https://linkedin.com"} icon={"logo-linkedin"} /> 
-      </div>
-    
-    
-    <Rodape>Myrella Putti</Rodape>
-  </div>
+      <Rodape>Myrella</Rodape>
+    </div>
   );
-};
+}
 
 export default App;
+
