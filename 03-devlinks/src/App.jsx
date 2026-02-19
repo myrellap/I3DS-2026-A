7ce22cee230d2b77d1f91c3da22ad4156d82babd
myrellap <myrellaputti2007@gmail.com>
2026-02-19 08:37:00 -0300

    Adicionados novos componentes: Link, Perfil, Rodapé, SocialLink e Switch com estilos
    - Criado o componente Link para renderizar links com estilos.
    
    - Adicionado o componente Perfil para exibir o perfil do usuário com imagem e nome de usuário.
    
    - Implementado o componente Rodapé para rodapé com mensagem personalizável.
    
    - Introduzido o componente SocialLink para ícones de redes sociais.
    
    - Desenvolvido o componente Switch para alternar temas com animações.
    
    - Adicionados módulos CSS correspondentes para cada componente para gerenciar estilos.

--- 03-devlinks/src/App.jsx
+++ 03-devlinks/src/App.jsx
@@ -1,41 +1,44 @@
+import { useState } from "react";
+import "./App.css";
+import Link from "./components/Link/Link";
+import Perfil from "./components/Perfil/Perfil";
+import Rodape from "./components/Rodape/Rodape";
+import SocialLink from "./components/SocialLink/SocialLink";
+import Switch from "./components/Switch/Switch";
 
-import './App.css'
-import Link from './assets/components/Link/Link';
-import Perfil from './assets/components/Perfil/Perfil';
-import SocialLink from './assets/components/SocialLink/SocialLink';
-import Rodape from './assets/components/Rodape/Rodape';
-import Switch from './assets/components/Switch/Switch';
-
+import foto from "./assets/JUSTIN-BIEBER.jpg";
 
 function App() {
+  const [isLight, setIsLight] = useState(true);
+
+  const troca = () => {
+    setIsLight(!isLight);
+  };
 
   return (
-   <div id= "App">
- <Perfil fotoPerfil={"https://placehold.co/100"}>Myrella</Perfil>
+    <div id="App" className={isLight ? "light" : ""}>
+      <Perfil fotoPerfil={foto}>Justin Bieber</Perfil>
 
+      <Switch troca={troca} isLight={isLight} />
 
-       <Switch />
-
-<div id= "Link">
-      <ul>
-        <Link url={""}>Inscreva-se</Link>
-        <Link url={""}>Minha Playlist</Link>
-        <Link url={""}>Me pague um lanche</Link>
-        <Link url={""}>Conheça o curso de DEV</Link>
-      </ul>
+      <div id="Link">
+        <ul>
+          <Link url={""}>Inscreva-se</Link>
+          <Link url={""}>Minha playlist</Link>
+          <Link url={""}>Me pague um café!</Link>
+          <Link url={""}>Conheça o Curso DEV</Link>
+        </ul>
       </div>
 
-      <div id='SocialLinks'>
-       <SocialLink url={"https://github.com"} icon = {"logo-github"} />
-       <SocialLink url={"https://instagram.com"} icon = {"logo-instagram"} />
-       <SocialLink url={"https://youtube.com"} icon = {"logo-youtube"} />
-       <SocialLink url={"https://br.linkedin.com/"} icon = {"logo-linkedin"} />
-       </div>
-
-      <Rodape>Myrella</Rodape>
+      <div id="SocialLinks">
+        <SocialLink url={"https://github.com"} icon={"logo-github"} />
+        <SocialLink url={"https://instagram.com"} icon={"logo-instagram"} />
+        <SocialLink url={"https://youtube.com"} icon={"logo-youtube"} />
+        <SocialLink url={"https://br.linkedin.com/"} icon={"logo-linkedin"} />
+      </div>
+      <Rodape>ProfCastello</Rodape>
     </div>
   );
 }
 
 export default App;
-
--- 03-devlinks/vite.config.js
+++ 03-devlinks/vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port: 3000,//muda o padrao vite(5173) para o padrao react(3000)
-    open: true, //Abra o navegador automaticamente
-    host: true, // permite que tenha acesso via IP na rede local
+  server: {
+    port: 3000, //Muda a porta padrão vite(5173) para a padrão react(3000)
+    open: true, //Abre o navegador automaticamente
+    host: true, //Permite que tenha acesso via IP na rede local
   },
 });
