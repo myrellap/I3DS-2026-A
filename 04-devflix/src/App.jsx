59b7e3f2b94b7eb9baa33652af74afcffc6729b6
myrellap <myrellaputti2007@gmail.com>
2026-02-19 13:58:50 -0300

    Funcionalidade: Adição de componente e estilos de rodapé, inclusão de ícone SVG de pesquisa
    - Criado um novo componente de rodapé (Rodape) com link e texto personalizáveis.
    
    - Adicionado módulo CSS correspondente para estilizar o rodapé.
    
    - Introduzido um novo ícone SVG de pesquisa para uso no aplicativo.

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+import logo from "./assets/devflix.png";
+import lupa from "./assets/search (1).svg";
+import Rodape from "./components/Rodape/Rodape";
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div id="App">
+      <img
+        className="logo"
+        src={logo}
+        alt="Imagem do logo do serviço de streaming Netflix, conhecido internacionalmente, com letras vermelhas sobre fundo preto, promovendo conteúdo de streaming."
+      />
 
-export default App
+      <div className="search">
+        <input type="text" placeholder="Pesquise por filmes e séries..." />
+        <img src={lupa} alt="Botão de ação para pesquisa!" />
+      </div>
+      
+      <Rodape link={"https://github.com/myrellap"}>Myrella</Rodape>
+    </div>
+  );
+};
+
+export default App;
